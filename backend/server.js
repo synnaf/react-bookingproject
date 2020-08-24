967b77d621d7da0f1b8ac6bb9703cbcb74ff0178
ninasse <58419590+ninasse@users.noreply.github.com>
2020-08-24 11:10:19 +0200

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const dbOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+};
+
 app.use(cors());
 app.use(express.json());
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+}); */
+
+const uri = process.env.MONGO_DB;
+mongoose.connect(uri, dbOptions).then(() => {
+  app.listen(PORT, () =>
+    console.log(`App listening on port ${PORT} and MongooDb connection OK!`)
+  );
 });
