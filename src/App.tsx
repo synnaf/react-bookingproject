a47670b5fa502a48be4296755343de388f2f078b
ninasse <58419590+ninasse@users.noreply.github.com>
2020-08-26 12:21:43 +0200

    push test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import Home from './components/home/Home';
+import React from "react";
+import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>>
+      <Home></Home>
     </div>
   );
 }
