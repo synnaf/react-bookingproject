68620257970576eedff3ff804864e2b0babde7c7
Llindq <leoniversum@hotmail.com>
2020-08-26 12:55:49 +0200

    Fixed spaces in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,70 +8,39 @@ function App() {
   return (
     <div className="App">
             
-      <React.Fragment>
-        <h1>Hej!</h1>
-            
-        <Router>
-                
-          <div>
-                    
-            <nav>
-                        
-              <ul>
-                            
+      <React.Fragment>        
+        <Router>           
+          <div>                   
+            <nav>                       
+              <ul>                            
                 <li>
-                                <Link to="/">Home</Link>
-                              
-                </li>
-                            
+                   <Link to="/">Home</Link>                             
+                </li>                         
                 <li>
-                                <Link to="/booking">Bookings</Link>
-                              
-                </li>
-                            
+                   <Link to="/booking">Bookings</Link>                          
+                </li>                         
                 <li>
-                                <Link to="/admin">Admin</Link>
-                              
-                </li>
-                          
-              </ul>
-                      
-            </nav>
-                    
-            <Switch>
-                        
-              <Route path="/home">
-                            
-                <Home />
-                          
-              </Route>
-                        
-              <Route path="/booking">
-                            
-                <Booking />
-                          
-              </Route>
-                        
-              <Route path="/admin">
-                            
-                <Admin />
-                          
-              </Route>
-                        
-              <Route path="/">
-                            
-                <Home />
-                          
-              </Route>
-                      
-            </Switch>
-                  
-          </div>
-              
-        </Router>
-            
-      </React.Fragment>
-              
+                   <Link to="/admin">Admin</Link>                      
+                </li>                  
+              </ul>              
+            </nav>             
+            <Switch>   
+              <Route path="/home">                           
+                <Home />                         
+              </Route>                       
+              <Route path="/booking">                           
+                <Booking />                      
+              </Route>                    
+              <Route path="/admin">                         
+                <Admin />                    
+              </Route>                   
+              <Route path="/">                       
+                <Home />                         
+              </Route>                     
+            </Switch>                
+          </div>            
+        </Router>           
+      </React.Fragment>         
     </div>
   );
 }
