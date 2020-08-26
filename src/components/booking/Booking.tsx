090ac5e47500bd08494fc05eb2e95dd155db76d4
Llindq <leoniversum@hotmail.com>
2020-08-26 11:25:24 +0200

    new branch copied from styling

--- src/components/booking/Booking.tsx
+++ src/components/booking/Booking.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import './Booking.scss'; 
 
 export default function Booking() {
-  return <div>Booking Works</div>;
+  return (
+    <div className="placeholder">
+      <form>
+        <fieldset className="input-container">
+          <label>Välj datum</label>
+            <input type="date" />
+        </fieldset>
+        <fieldset className="select-container">
+          <label>Välj tid</label>
+          <select>
+            <option>18.00</option>
+            <option>21.00</option>
+          </select>
+        </fieldset>
+        <fieldset className="select-container">
+          <label>Välj antal</label>
+          <select >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+          </select>
+        </fieldset>
+        <button type="button">Sök</button>
+      </form>
+    
+
+  </div>
+  );
 }
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import './Home.scss'; 
+import Sushi from './sushi.jpg'; 
 
 export default function Home() {
-  return <div>Home Works!!!</div>;
+  return (
+    <div className="placeholder">
+
+      
+
+      <div className="image-container">
+        <img src={Sushi} alt="Placeholder img" />
+      </div>
+  
+      <button>Boka</button>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse 
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+        anim id est laborum.
+      </p>
+
+    </div>
+  );
 }
