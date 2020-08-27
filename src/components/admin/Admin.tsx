a41e25b69cde09411a0c100297635b2c2991044d
Llindq <leoniversum@hotmail.com>
2020-08-27 17:01:36 +0200

    First axios commit

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import axios from 'axios';
 import './Admin.scss'; 
 
 export default function Admin() {
+
+  axios.get('localhost:3001/bookings/').then
+        (data => {
+            console.log(data);
+        })
+
   return (
     <div className="main-container">
       <div className="placeholder">Admin Works</div>
