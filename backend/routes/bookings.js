3886ed97d215731e7cff754d7cda2f9557f81d39
ninasse <58419590+ninasse@users.noreply.github.com>
2020-08-27 19:17:29 +0200

    changes in get bookings request

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,20 +1,27 @@
 let Booking = require("../models/booking.model");
 let Guest = require("../models/guest.model");
-let allGuests = require("../routes/guests");
+const { getDefaultSettings } = require("http2");
 const router = require("express").Router();
 
 router.route("/").get((req, res) => {
-  let allReservations = Booking.find()
+  Booking.find()
     .then((bookings) => res.json(bookings))
     .catch((err) => res.status(400).json("Error:" + err));
-  console.log(allReservations);
 });
 
-/* router.route("/availability").get((req, res) => {
-    get lista med bokningar filtrerat på datum
-  }); */
+router.route("/availability").get((req, res) => {
+  /* get lista med bokningar filtrerat på datum */
 
-router.route("availability/addbooking").post((req, res) => {
+  let requiredDate = "2020-08-27";
+
+  Booking.find({
+    date: requiredDate /* req.body.date */,
+  })
+    .then((bookings) => res.json(bookings))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
+router.route("/availability/addbooking").post((req, res) => {
   const name = req.body.name;
   const lName = req.body.lName;
   const email = req.body.email;
@@ -35,18 +42,18 @@ router.route("availability/addbooking").post((req, res) => {
     phoneNumber: 123,
   });
   const newBooking = new Booking({
-    bookingId: 12,
-    date: Date.now(),
+    bookingId: 13,
+    date: "2020-08-27T00:00:00.000Z" /* Date().toString() */,
     time: "18",
     seats: 2,
-    notes: "",
+    notes: "Vegan",
     guestId: 2,
   });
 
   /* spara bokning med värden hämtat från req.body */
   newBooking
     .save()
-    .then(() => res.json("Booking added!"))
+    .then(() => res.json(newBooking))
     .catch((err) => res.status(400).json("Error:" + err));
 
   /* spara gäst med värden hämtat från formulär */
