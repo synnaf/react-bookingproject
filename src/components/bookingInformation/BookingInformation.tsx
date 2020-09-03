54c13206c1f2951fcd37216c522fb015638a85b1
[synnaf] <[f.vforsman@gmail.com]>
2020-09-03 10:32:39 +0200

    Working version of bookingform

--- src/components/bookingInformation/BookingInformation.tsx
+++ src/components/bookingInformation/BookingInformation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useReducer } from "react";
 import './BookingInformation.scss'; 
 import axios from 'axios'; 
 import Booking from '../../models/Booking';
@@ -9,72 +9,63 @@ export interface IBookingInformationProps {
 
 export default function BookinInformation(props: IBookingInformationProps) {
 
-
 // TVÅ ST STATES VARS UPPDRAG ÄR ATT villkora rendering
-  const [isAvailable, setIsAvailable] = useState(true);
-  const [findTime, setFindTime] = useState(false);
-  const [requestedDate, setRequestedDate] = useState('');  
-  const [newTable, setNewTable] = useState(new Booking()); 
+  const [isAvailable18, setIsAvailable18] = useState(true);
+  const [isAvailable21, setIsAvailable21] = useState(true);
+  const [findTime, setFindTime] = useState(true);
+
+  let defaultValue: Booking = {bookingId: 0, time: '', date: '', seats: 0, notes: '', guestId: 33}; 
+  const [test, setTest] = useReducer(
+    (test: Booking, newTest: Booking) => ({
+      ...test, ...newTest
+    }), defaultValue
+  );
+
+  function update(e: ChangeEvent<any>) {
+    let element = e.target.name; 
+    let value = e.target.value; 
+    setTest({[element]: value} as any); 
+  }
+
 
 // FUNKTION FÖR: att villkora rendering
   function showTimeSlots() { 
     setFindTime(true);
   }
-
   
-//FUNKTION FÖR: hämta input-värden, spara i lokalt state 
-  function handleInput(e: ChangeEvent<any>) {
-    let element = e.target.name; 
-    let value = e.target.value;  
-    setRequestedDate({[element]: value} as any); 
-  }
-
-
 //FUNKTION FÖR: hantera värdena när man trycker på knappen
-  function createSearch(e: ChangeEvent<any>) {    
+  function createSearch() {    
     showTimeSlots(); //uppdatera state för 18 och 21
-    handleInput(e); // uppdatera formulärstate 
+    let selectedDate = test.date; 
+    let selectedSeats = test.seats; 
 
-    console.log(requestedDate); //statet för datum antal, tid 
-    let selectedDate = requestedDate;  // hämta datumet dynamiskt
-    console.log("selected date = " + selectedDate); 
-    
     //vi mappar ut för kl.18 och kl.21 
         axios.get('http://localhost:3001/bookings/')
         .then(currentBookings => { 
           let allBookings = currentBookings.data;  
-
-            // loopa igenom listan och HITTA en matchning 
-            //vad är chosenDate för ngt? det är datumet från databasen...? 
-              let newArr = allBookings.filter((chosenDate: any) => chosenDate.date.includes(selectedDate)); 
-              console.log(newArr);
-
-            // kontrollera längden för valt datum
-              if (newArr.length > 29) {
-                console.log("fullbokat!!!")
-                return setFindTime(false); 
-
-              } else {
-                setFindTime(true)
-              
-                //när längden är färre än 29 finns det platser, leta efter tider: 
-                let timeSlotEarly = newArr.filter((t: Booking)=> t.time.includes('18')); 
-                let timeSlotLate = newArr.filter((t: Booking)=> t.time.includes('21'));
-                console.log(timeSlotEarly); 
-                console.log(timeSlotLate); //denna blir tom eftersom inga bokningar finns
-                
-                timeSlotEarly.length > 14 ? setIsAvailable(false) : setIsAvailable(true);  
-                timeSlotLate.length > 14 ? setIsAvailable(false) :  setIsAvailable(true);
-
-              }
-              
-            });
+          let newArr = allBookings.filter((chosenDate: any) => chosenDate.date.includes(selectedDate)); 
+          
+          // kontrollera längden för valt datum
+          if (newArr.length > 29) {
+            console.log("fullbokat!!!")
+            console.log(newArr); 
+            return setFindTime(false); 
+          } else {
+            setFindTime(true); 
+            let timeSlotEarly = newArr.filter((t: Booking)=> t.time.includes('18')); 
+            let timeSlotLate = newArr.filter((t: Booking)=> t.time.includes('21'));
+            
+            timeSlotEarly.length > 14 ? setIsAvailable18(false) : setIsAvailable18(true);  
+            timeSlotLate.length > 14 ? setIsAvailable21(false) :  setIsAvailable21(true);
+          }    
+        });
   }
 
-  function searchForTable() {
-    console.log("test"); 
-    props.addBooking(newTable); //skicka state objektet  
+  function sendBooking() {
+    console.log(test);
+    props.addBooking(test); 
   }
+
 
   return (
     <div className="placeholder">
@@ -82,11 +73,11 @@ export default function BookinInformation(props: IBookingInformationProps) {
       <form>
         <fieldset className="input-container">
           <label>Välj datum</label>
-            <input type="date" onChange={handleInput} name="date"/>
+            <input type="date" onChange={update} name="date"/>
         </fieldset>
         <fieldset className="select-container">
           <label>Välj antal</label>
-          <select name="seats" onChange={handleInput}>
+          <select name="seats" onChange={update}>
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -95,48 +86,40 @@ export default function BookinInformation(props: IBookingInformationProps) {
             <option>6</option>
           </select>
         </fieldset>
-        <div className="cta-search" onClick={createSearch}>
+        <div className="cta-search">
           <button type="button" onClick={createSearch}>Sök</button> 
         </div>
       </form>
       
       {findTime ?
-
         <div className="find-time">
           <fieldset className="select-time">         
-              <label>Lediga tider:</label>
-              
-              {isAvailable ? 
-              
-              <div>
-                <input type="radio" id="18" name="time" value="18" onChange={handleInput}/>
-                <label htmlFor="18">18.00</label>
-              </div>
-            : <div>
-                <h4>SORRY 18</h4>
-              </div>  
-            }
-            
-            {isAvailable ? 
-
-              <div>
-                <input type="radio" id="21" name="time" value="21" onChange={handleInput}/>
-                <label htmlFor="21">21.00</label>
-              </div>
-
-            : <div>
-                <h4>SORRY 21</h4>
-              </div> 
-            }
-
-
-            </fieldset>
+              <label>Välj tid:</label>
+                             
+              {isAvailable18 ? 
+                <div>
+                  <input type="radio" id="18" name="time" value="18" onChange={update}/>
+                  <label htmlFor="18">18.00</label>
+                </div>
+              : <div>
+                <h4>18.00 FULLT</h4>
+                </div>  
+              }
+              {isAvailable21 ?
+                <div>
+                  <input type="radio" id="21" name="time" value="21" onChange={update}/>
+                  <label htmlFor="21">21.00</label>
+                </div>
+              : <div>
+                  <h4>21.00 FULLT</h4>
+                </div> 
+              }
+          </fieldset>
             <div className="cta-form">
-              <button type="button" onClick={() => searchForTable()}>Nästa</button> 
+              <button type="button" onClick={() => sendBooking()}>Nästa</button> 
             </div> 
           </div> 
-            : null
-        }
+        : <h1>FULLT</h1> }
       </div>
     </div>
   );
--- src/components/createBooking/CreateBooking.tsx
+++ src/components/createBooking/CreateBooking.tsx
@@ -7,18 +7,25 @@ import GuestInformation from '../guest-information/GuestInformation';
 
 export default function CreateBooking() {
     
-    const [reservation, setReservation] = useState(new Booking()); 
+    //VÅRT TABLE-STATE, kommer bestå av ett boka-objekt. 
+    const [reservation, setReservation] = useState({bookingId: 0, time: '', date: '', seats: 0, notes: '', guestId: 33}); 
+
+    //VÅR GUEST-STATE, kommer bestå av ett gäst-objekt 
     const [guest, setGuest] = useState(new Guest()); 
 
 
     //FUNKTION FÖR ATT SPARA BOKNINGEN
-    function saveBooking(test: Booking) {
-        console.log(test); 
+    // vår funktion tar emotr ett objekt som är som interfacet 
+    function saveBooking(booking: Booking) {
+        setReservation(booking);
     }
   
+    //FUNKTION FÖR ATT SPARA GÄSTEN 
     function saveGuest() {
       console.log("Gäst sparad i db");
     }
+
+    console.log(reservation); 
 
     return(
         <div>
