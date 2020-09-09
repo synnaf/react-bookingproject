083a762997119f00250aa2e865dd71d666ad89fb
[synnaf] <[f.vforsman@gmail.com]>
2020-09-09 21:46:12 +0200

    removed unused test

--- src/components/guest-information/GuestInformation.test.tsx
+++ src/components/guest-information/GuestInformation.test.tsx
@@ -10,12 +10,6 @@ test('render contents of booking form', () => {
     const {getByText} = render(<GuestInformation addGuest={saveGuest} addNotes={saveNotes}></GuestInformation>);
         const labelElement = getByText(/Fyll i e-mailadress/i);
         const buttonElement = getByText(/Nästa/i);
-        // const contactElement = getByText(/Kontaktuppgifter:/i);
-        // const otherElement = getByText(/Övrig information/i);
-        // const gdprElement = getByText(/Boka/i);
         expect(labelElement).toBeInTheDocument();
         expect(buttonElement).toBeInTheDocument();
-        // expect(contactElement).toBeInTheDocument();
-        // expect(otherElement).toBeInTheDocument();
-        // expect(gdprElement).toBeInTheDocument();
 });
