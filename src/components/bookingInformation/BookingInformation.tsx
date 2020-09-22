a7790b9563aecf804f4afe2e0cecd61038748985
[synnaf] <[f.vforsman@gmail.com]>
2020-09-22 11:27:19 +0200

    adding ciel+filter calc for tables time 18

--- src/components/bookingInformation/BookingInformation.tsx
+++ src/components/bookingInformation/BookingInformation.tsx
@@ -2,6 +2,7 @@ import React, { useState, ChangeEvent, useReducer } from "react";
 import "./BookingInformation.scss";
 import axios from "axios";
 import Booking from "../../models/Booking";
+import { isNull } from "util";
 
 export interface IBookingInformationProps {
   addBooking(booking: Booking): void;
@@ -30,72 +31,74 @@ export default function BookinInformation(props: IBookingInformationProps) {
   function createSearch() {
 
     //funktion för att skapa en sökning, inkludera öppet val av platser 
+    //steg 1: kontrollera antal rader i DB för valt datum (max 15) 
+    //steg 2: räkna ut hur många platser som är upptagna 
 
     setFindTime(true);
 
     let selectedDate = bookingInfo.date;
     let selectedSeats = bookingInfo.seats; 
-    console.log(selectedSeats); 
 
     axios.get("http://localhost:3001/bookings/").then((currentBookings) => {
-    
-    //hämta alla bokningar
-    let allBookings = currentBookings.data;
+      //hämta alla bokningar
+      let allBookings = currentBookings.data;
       let newArr = allBookings.filter((chosenDate: any) =>
         chosenDate.date.includes(selectedDate)
       );
 
-      //om det finns fler än 29, fullbokat
       if (newArr.length > 29) {
         setFindTime(false);
         setFullyBooked(true);
 
       } else {
-        //om det finns färre rader än 29, finns det lediga bord 
         setFindTime(true);
         setFullyBooked(false);
 
-        //skapa en array av bokningarna som innehåller 18
-        //loopa igenom på de som matchar datum + tid 
-        let timeSlotEarly = newArr.filter((t: Booking) =>
-          t.time.includes("18") //hittar de som innehåller 18 och sparar i timeSlotEarly
-        );
-        timeSlotEarly.map((time: any) => {
-          let table = 0; 
+        //skapa en array av bokningarna som innehåller kl.18
+        let timeSlotEarly = newArr.filter((t: Booking) => t.time.includes("18"));
+
+        //Utav tiderna kl.18, räkna ut hur många bord som är upptagna 
+        // totalt antal platser: 15 x 6 = 90 
+        let tableEarly = 0; 
+        timeSlotEarly.map((bookedTable: any) => {
+          
           // 28:e 2 bokningar 10 pers + 3 pers kräver 3 bord
-          // totalt antal platser: 15 x 6 = 90 
-          let amT = Math.ceil(time.seats / 6); //rundar upp till heltal
+          //rundar upp till heltal
+          console.log(bookedTable); 
+          let amT = Math.ceil(bookedTable.seats / 6); 
           console.log("AMOUNT OF TABLES IS" + amT); 
-          return (table += amT); 
+          return (tableEarly += amT); //här uppdateras värdet 
         }); 
 
-        //skapa en array av bokningar som innehåller 21 
-        let timeSlotLate = newArr.filter((t: Booking) => 
-          t.time.includes("21")
-        );
+        //visar korrekt antal bord den dagen 
+        console.log("TABLE 18 is " + tableEarly); 
+        //hur räknar jag ut om det finns ledigt bord? nu hittar jag ju bara upptagna bord? 
 
-        //här ändrar vi värdet på variabeln tables 
-        let table = 0; 
-        timeSlotLate.map((time: any) => {
-          let amT = Math.ceil(time.seats / 6);
-          console.log("AMOUNT OF TABLES IS" + amT); 
-          return (table += amT); 
-        });
-        
         //om det finns ledigt kl 18
-        table < 15 
-          ? setIsAvailable18(true)
-          : setIsAvailable18(false); 
+        tableEarly < 15 
+        ? setIsAvailable18(true) 
+        : setIsAvailable18(false); 
+
 
         
-        // timeSlotEarly.length > 14
-        //   ? setIsAvailable18(false)
-        //   : setIsAvailable18(true);
+        // //skapa en array av bokningar som innehåller 21 
+        // let timeSlotLate = newArr.filter((t: Booking) => t.time.includes("21"));
+        // //här ändrar vi värdet på variabeln tables 
+        // let tableLate = 0; 
+        // timeSlotLate.map((time: any) => {
+        //   let amT = Math.ceil(time.seats / 6);
+        //   console.log("AMOUNT OF TABLES IS" + amT); 
+        //   return (tableLate += amT); 
+        // });
 
-        //om det finns ledigt kl 21 
-        timeSlotLate.length > 14
-          ? setIsAvailable21(false)
-          : setIsAvailable21(true);
+        // console.log("TABLE 21 is " + tableLate); //visar korrekt antal bord den dagen 
+        // //man kan bara boka max X antal platser när det är X bord upptagna. 
+
+        // //om det finns ledigt kl 21 
+        // tableLate < 15 
+        // ? setIsAvailable21(true)
+        // : setIsAvailable21(false); 
+
       }
     });
   }
@@ -112,11 +115,13 @@ export default function BookinInformation(props: IBookingInformationProps) {
             <label id="date">Välj datum</label>
             <input type="date" onChange={update} name="date" aria-label="date" />
           </fieldset>
+
           <fieldset className="select-container">
             {/* adding new type of input field */}
             <label>Välj antal</label>
             <input type="number" placeholder="1" onChange={update} name="seats"/>
           </fieldset>
+
           {bookingInfo.date === "" ? (
             <div className="cta-search">
               <button type="button" disabled>
@@ -130,6 +135,7 @@ export default function BookinInformation(props: IBookingInformationProps) {
               </button>
             </div>
           )}
+
           {fullyBooked ? (
             <h1>fullbokat</h1>
           ) : findTime ? (
@@ -173,6 +179,7 @@ export default function BookinInformation(props: IBookingInformationProps) {
               </fieldset>
             </div>
           ) : null}
+
         </form>
       </div>
     </div>
